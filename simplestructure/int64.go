ba1e2fd57a9c1cd3d0aea1e046cfad587e3f5418
guantingwei <guantingwei@sixents.com>
2020-11-16 10:34:19 +0800

    新增 Int64 类型支持

---
+++ simplestructure/int64.go
@@ -0,0 +1,84 @@
+/**
+@Author: wei-g
+@Date:   2020/11/13 10:02 上午
+@Description: 一些基本的数据类型封装
+*/
+
+package simplestructure
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+// Int64Str
+// 实现主要来源于 https://stackoverflow.com/questions/49415573/golang-json-how-do-i-unmarshal-array-of-strings-into-int64?rq=1
+type Int64 int64
+
+// MarshalJSON 满足json 序列化接口
+func (i Int64) MarshalJSON() ([]byte, error) {
+	return json.Marshal(strconv.FormatInt(int64(i), 10))
+}
+
+// UnmarshalJSON 满足json 反序列化接口
+func (i *Int64) UnmarshalJSON(b []byte) (err error) {
+	var (
+		s     string
+		value int64
+	)
+	// Try string first
+	if err := json.Unmarshal(b, &s); err != nil {
+		// Fallback to number
+		return json.Unmarshal(b, (*int64)(i))
+	}
+	if value, err = strconv.ParseInt(s, 10, 64); err != nil {
+		return
+	}
+	*i = Int64(value)
+	return
+}
+
+// ToBuiltinInt64Slice Int64类型切片转换为 系统内置标准的int64类型切片
+func ToBuiltinInt64Slice(slice []Int64) (int64Slice []int64) {
+	if slice == nil {
+		return nil
+	}
+	var tmp []Int64
+
+	if len(slice) <= 1024 { // 判断数据是否过大 如果过大则不进行数据copy
+		// 拷贝数据, 防止其他协程进行修改
+		tmp = make([]Int64, len(slice), cap(slice))
+		copy(tmp, slice)
+	} else {
+		tmp = slice
+	}
+
+	int64Slice = make([]int64, 0, cap(tmp))
+	// 进行数据转换
+	for _, v := range tmp {
+		int64Slice = append(int64Slice, int64(v))
+	}
+	return
+}
+
+// ToInt64Slice  系统内置标准的int64类型切片 转换为Int64类型切片
+func ToInt64Slice(int64Slice []int64) (slice []Int64) {
+	if int64Slice == nil {
+		return nil
+	}
+	var tmp []int64
+	if len(int64Slice) <= 1024 { // 判断数据是否过大 如果过大则不进行数据copy
+		// 拷贝数据, 防止其他协程进行修改
+		tmp = make([]int64, len(int64Slice), cap(int64Slice))
+		copy(tmp, int64Slice)
+	} else {
+		tmp = int64Slice
+	}
+
+	slice = make([]Int64, 0, cap(tmp))
+	for _, v := range tmp {
+		slice = append(slice, Int64(v))
+	}
+	return
+
+}
---
+++ simplestructure/int64_test.go
@@ -0,0 +1,198 @@
+/**
+@Author: wei-g
+@Date:   2020/11/13 10:32 上午
+@Description:
+*/
+
+package simplestructure
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+type TestFoo struct {
+	Bars []Int64 `json:"bars"`
+	Name string  `json:"name"`
+	ID   Int64   `json:"id"`
+}
+
+// TestInt64Str_MarshalJSON 测试 Int64 在进行json 序列化时 是否符合预期
+func TestInt64Str_MarshalJSON(t *testing.T) {
+	type test struct { // 定义test结构体
+		want  string
+		input interface{}
+	}
+	tests := map[string]test{
+		"base": {want: `"12"`,
+			input: Int64(12),
+		},
+		"negative": {want: `"-12"`,
+			input: Int64(-12),
+		},
+		"slice": {want: `["1","2","3"]`,
+			input: []Int64{1, 2, 3},
+		},
+		"struct": {want: `{"bars":["1","2","3","4"],"name":"one"}`,
+			input: struct {
+				Bars []Int64 `json:"bars"`
+				Name string  `json:"name"`
+			}{
+				Bars: []Int64{1, 2, 3, 4},
+				Name: "one"},
+		},
+		"struct_simple": {
+			want: `{"bars":"1","name":"one"}`,
+			input: struct {
+				Bars Int64  `json:"bars"`
+				Name string `json:"name"`
+			}{
+				Bars: Int64(1),
+				Name: "one"},
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := json.Marshal(tc.input)
+			if err != nil {
+				t.Errorf("input: %s, 出现异常错误: %s\n", tc.input, err)
+				return
+			}
+			if string(got) != tc.want {
+				t.Errorf("excepted: %s, got: %s", tc.want, got)
+			}
+		})
+	}
+}
+
+// TestInt64Str_UnmarshalJSON 测试 Int64 在进行json 反序列化时 是否符合预期
+func TestInt64Str_UnmarshalJSON(t *testing.T) {
+	type test struct { // 定义test结构体
+		want  Int64
+		input string
+	}
+
+	tests := map[string]test{
+		"base":            {want: Int64(12), input: `12`},
+		"symbol":          {want: Int64(12), input: `"12"`},
+		"negative":        {want: Int64(-12), input: `-12`},
+		"symbol_negative": {want: Int64(-12), input: `"-12"`},
+		"big":             {want: Int64(9223372036854775807), input: "9223372036854775807"},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			var i Int64
+			err := json.Unmarshal([]byte(tc.input), &i)
+			if err != nil {
+				t.Errorf("input: %s, 出现异常错误: %s\n", tc.input, err)
+				return
+			}
+			if i != tc.want {
+				t.Errorf("excepted: %d, got: %d", tc.want, i)
+			}
+		})
+	}
+
+}
+
+// TestInt64_UnmarshalJSONTestFoo 测试 Int64 在结构体中序列化 表现
+func TestInt64_UnmarshalJSONTestFoo(t *testing.T) {
+	type test struct {
+		input string
+		want  TestFoo
+	}
+	tests := map[string]test{
+		"nil_slice": {
+			input: `{"bars":null,"name":"one","id":1}`,
+			want: TestFoo{
+				Bars: nil,
+				Name: "one",
+				ID:   1,
+			},
+		},
+		"empty_slice": {
+			input: `{"bars":[],"name":"two","id":"2"}`,
+			want: TestFoo{
+				Bars: []Int64{},
+				Name: "two",
+				ID:   2,
+			},
+		},
+		"slice": {
+			input: `{"bars":["1","2","3", "4",5],"name":"three","id":"-22000"}`,
+			want: TestFoo{
+				Bars: []Int64{1, 2, 3, 4, 5},
+				Name: "three",
+				ID:   -22000,
+			},
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			var got TestFoo
+			err := json.Unmarshal([]byte(tc.input), &got)
+			if err != nil {
+				t.Errorf("input: %s, 出现异常错误: %s\n", tc.input, err)
+				return
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf(" excepted:%#v, got:%#v", tc.want, got)
+			}
+			//t.Logf("got: %#v\n", got)
+		})
+	}
+
+}
+
+func TestToBuiltinInt64Slice(t *testing.T) {
+	type test struct {
+		input []Int64
+		want  []int64
+	}
+	tests := map[string]test{
+		"nil":   {nil, nil},
+		"empty": {[]Int64{}, []int64{}},
+		"short": {[]Int64{1, 2, 3, 4, 5}, []int64{1, 2, 3, 4, 5}},
+		"long":  {make([]Int64, 1024, 2048), make([]int64, 1024, 2048)},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := ToBuiltinInt64Slice(tc.input)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf(" excepted:%#v, got:%#v", tc.want, got)
+				return
+			}
+			if cap(got) != cap(tc.want) {
+				t.Errorf("cap unequal, excepted: %d, got: %d", cap(tc.want), cap(got))
+				return
+			}
+		})
+	}
+}
+
+func TestToInt64Slice(t *testing.T) {
+	type test struct {
+		input []int64
+		want  []Int64
+	}
+	tests := map[string]test{
+		"nil":   {nil, nil},
+		"empty": {[]int64{}, []Int64{}},
+		"short": {[]int64{1, 2, 3, 4, 5}, []Int64{1, 2, 3, 4, 5}},
+		"long":  {make([]int64, 1024, 2048), make([]Int64, 1024, 2048)},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := ToInt64Slice(tc.input)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf(" excepted:%#v, got:%#v", tc.want, got)
+				return
+			}
+			if cap(got) != cap(tc.want) {
+				t.Errorf("cap unequal, excepted: %d, got: %d", cap(tc.want), cap(got))
+				return
+			}
+		})
+	}
+}
