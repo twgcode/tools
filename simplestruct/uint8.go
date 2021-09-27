263668b3de564794483bed13c42ff23ba7871d4c
guantingwei <guantingwei@sixents.com>
2021-09-27 15:00:52 +0800

    add uint8 tool function 1. Remove duplicate elements from slices 2. Determines whether the specified element exists in the slice 3. Removes the specified element from the slice

---
+++ simplestruct/uint8.go
@@ -0,0 +1,45 @@
+/**
+@Author: wei-g
+@Date:   2021/9/23 11:02 上午
+@Description:
+*/
+
+package simplestruct
+
+// BuiltinUint8SliceContains 判断 item是发在指定的切片中
+func BuiltinUint8SliceContains(slice []uint8, val uint8) (ok bool) {
+	for _, v := range slice {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
+
+// BuiltinUint8SliceRemoveDuplicateValues 对切片进行去重
+func BuiltinUint8SliceRemoveDuplicateValues(slice []uint8) []uint8 {
+	if len(slice) == 0 {
+		return slice
+	}
+	list := make([]uint8, 0, cap(slice))
+	for _, item := range slice {
+		if !BuiltinUint8SliceContains(list, item) {
+			list = append(list, item)
+		}
+	}
+	return list
+}
+
+// BuiltinUint8SliceRemoveElement 从切片中移除遇到第一个指定元素
+func BuiltinUint8SliceRemoveElement(source []uint8, element uint8) ([]uint8, bool) {
+	if len(source) == 0 {
+		return source, false
+	}
+	for i, v := range source {
+		if v == element {
+			source = append(source[:i], source[i+1:]...)
+			return source, true
+		}
+	}
+	return source, false
+}
