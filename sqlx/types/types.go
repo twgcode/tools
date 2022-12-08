0935bc804bebf23db08c8cc0b254fdebf7687f3d
guantingwei <guantingwei@sixents.com>
2022-12-08 18:51:12 +0800

    feat(sqlx/types): 完善 对 sqlx/types NewSliceNullJSONText() 函数 逻辑, 新增 对 sqlx types.JSONText 数据结构提供一些便利的函数
    1. 使 NewSliceNullJSONText() 函数支持 字符串切片 指针
    1. 新增 NewEmptySliceJSONText 函数
    2. 新增 NewSliceJSONText 函数
    3. 新增 NewInt32SliceJSONText 函数
    4. 新增 NewInt64SliceJSONText 函数
    5. 新增 NewStringSliceJSONText 函数

--- sqlx/types/types.go
+++ sqlx/types/types.go
@@ -10,6 +10,7 @@ package types
 import (
 	"encoding/json"
 	"github.com/jmoiron/sqlx/types"
+	"reflect"
 )
 
 // NewEmptySliceNullJSONText 参加一个空的 切片的  types.NullJSONText
@@ -19,13 +20,36 @@ func NewEmptySliceNullJSONText() (result types.NullJSONText) {
 	return
 }
 
-// NewSliceNullJSONText 创建 一个 切片的 NullJSONText, 不关心 source 中元素的类型
+// NewSliceNullJSONText 创建 一个 切片的 types.NullJSONText, 不关心 source 中元素的类型
 func NewSliceNullJSONText(source interface{}, empty bool) (result types.NullJSONText, err error) {
-	var data []byte
-	if source == nil && empty {
+	// 如果 source 直接是nil
+	if empty && source == nil {
 		result = NewEmptySliceNullJSONText()
 		return
 	}
+	sourceType := reflect.TypeOf(source)
+	sourceTypeKind := sourceType.Kind()
+
+	// source 为 切片, 比如 var source []string
+	if empty && (sourceTypeKind == reflect.Slice || sourceTypeKind == reflect.Array) {
+		if reflect.ValueOf(source).Len() == 0 {
+			result = NewEmptySliceNullJSONText()
+			return
+		}
+	}
+	// source 为指针 但是，指向的是 切片 比如  var source []string 调用时 NewSliceNullJSONText(&source)
+	// 或者 var source *[]string
+	if sourceTypeKind == reflect.Ptr {
+		sourceTypeKind = sourceType.Elem().Kind()
+		if empty && (sourceTypeKind == reflect.Slice || sourceTypeKind == reflect.Array) {
+			if reflect.ValueOf(source).Elem().Len() == 0 {
+				result = NewEmptySliceNullJSONText()
+				return
+			}
+		}
+	}
+
+	var data []byte
 	if data, err = json.Marshal(source); err != nil {
 		return
 	}
@@ -36,13 +60,13 @@ func NewSliceNullJSONText(source interface{}, empty bool) (result types.NullJSON
 	return
 }
 
-// NewInt32SliceNullJSONText 创建 一个 int32 切片的 NullJSONText
+// NewInt32SliceNullJSONText 创建 一个 int32 切片的 types.NullJSONText
 func NewInt32SliceNullJSONText(source []int32, empty bool) (result types.NullJSONText, err error) {
-	var data []byte
 	if len(source) == 0 && empty {
 		result = NewEmptySliceNullJSONText()
 		return
 	}
+	var data []byte
 	if data, err = json.Marshal(source); err != nil {
 		return
 	}
@@ -53,13 +77,13 @@ func NewInt32SliceNullJSONText(source []int32, empty bool) (result types.NullJSO
 	return
 }
 
-// NewInt64SliceNullJSONText 创建 一个 int64 切片的 NullJSONText
+// NewInt64SliceNullJSONText 创建 一个 int64 切片的 types.NullJSONText
 func NewInt64SliceNullJSONText(source []int64, empty bool) (result types.NullJSONText, err error) {
-	var data []byte
 	if len(source) == 0 && empty {
 		result = NewEmptySliceNullJSONText()
 		return
 	}
+	var data []byte
 	if data, err = json.Marshal(source); err != nil {
 		return
 	}
@@ -70,13 +94,13 @@ func NewInt64SliceNullJSONText(source []int64, empty bool) (result types.NullJSO
 	return
 }
 
-// NewStringSliceNullJSONText 创建 一个 string 切片的 NullJSONText
+// NewStringSliceNullJSONText 创建 一个 string 切片的 types.NullJSONText
 func NewStringSliceNullJSONText(source []string, empty bool) (result types.NullJSONText, err error) {
-	var data []byte
 	if len(source) == 0 && empty {
 		result = NewEmptySliceNullJSONText()
 		return
 	}
+	var data []byte
 	if data, err = json.Marshal(source); err != nil {
 		return
 	}
@@ -84,5 +108,99 @@ func NewStringSliceNullJSONText(source []string, empty bool) (result types.NullJ
 		return
 	}
 	result.Valid = true
+	return
+}
+
+// NewEmptySliceJSONText 参加一个空的 切片的  types.JSONText
+func NewEmptySliceJSONText() (result types.JSONText) {
+	_ = result.UnmarshalJSON([]byte{91, 93}) // 91 93 []
+	return
+}
+
+// NewSliceJSONText 创建 一个 切片的 types.NullJSONText, 不关心 source 中元素的类型
+func NewSliceJSONText(source interface{}, empty bool) (result types.JSONText, err error) {
+	// 如果 source 直接是nil
+	if empty && source == nil {
+		result = NewEmptySliceJSONText()
+		return
+	}
+	sourceType := reflect.TypeOf(source)
+	sourceTypeKind := sourceType.Kind()
+
+	// source 为 切片, 比如 var source []string
+	if empty && (sourceTypeKind == reflect.Slice || sourceTypeKind == reflect.Array) {
+		if reflect.ValueOf(source).Len() == 0 {
+			result = NewEmptySliceJSONText()
+			return
+		}
+	}
+	// source 为指针 但是，指向的是 切片 比如  var source []string 调用时 NewSliceNullJSONText(&source)
+	// 或者 var source *[]string
+	if sourceTypeKind == reflect.Ptr {
+		sourceTypeKind = sourceType.Elem().Kind()
+		if empty && (sourceTypeKind == reflect.Slice || sourceTypeKind == reflect.Array) {
+			if reflect.ValueOf(source).Elem().Len() == 0 {
+				result = NewEmptySliceJSONText()
+				return
+			}
+		}
+	}
+
+	var data []byte
+	if data, err = json.Marshal(source); err != nil {
+		return
+	}
+	if err = result.UnmarshalJSON(data); err != nil {
+		return
+	}
+	return
+}
+
+// NewInt32SliceJSONText 创建 一个 int32 切片的 NullJSONText
+func NewInt32SliceJSONText(source []int32, empty bool) (result types.JSONText, err error) {
+	if len(source) == 0 && empty {
+		result = NewEmptySliceJSONText()
+		return
+	}
+	var data []byte
+	if data, err = json.Marshal(source); err != nil {
+		return
+	}
+	if err = result.UnmarshalJSON(data); err != nil {
+		return
+	}
+
+	return
+}
+
+// NewInt64SliceJSONText 创建 一个 int64 切片的 NullJSONText
+func NewInt64SliceJSONText(source []int64, empty bool) (result types.JSONText, err error) {
+	if len(source) == 0 && empty {
+		result = NewEmptySliceJSONText()
+		return
+	}
+	var data []byte
+	if data, err = json.Marshal(source); err != nil {
+		return
+	}
+	if err = result.UnmarshalJSON(data); err != nil {
+		return
+	}
+	return
+}
+
+// NewStringSliceJSONText 创建 一个 string 切片的 NullJSONText
+func NewStringSliceJSONText(source []string, empty bool) (result types.JSONText, err error) {
+	if len(source) == 0 && empty {
+		result = NewEmptySliceJSONText()
+		return
+	}
+	var data []byte
+	if data, err = json.Marshal(source); err != nil {
+		return
+	}
+	if err = result.UnmarshalJSON(data); err != nil {
+		return
+	}
 	return
 }
--- sqlx/types/types_test.go
+++ sqlx/types/types_test.go
@@ -52,3 +52,85 @@ func TestNewStringSliceNullJSONText(t *testing.T) {
 		})
 	}
 }
+
+type User struct {
+	Name string `json:"name"`
+}
+
+func TestNewSliceNullJSONText(t *testing.T) {
+	type test struct {
+		input interface{}
+		want  string
+	}
+	var strSlicePtrNil []string
+	var strSlicePtrEmpty = make([]string, 0, 0)
+	var structSlice = []User{{"x"}}
+
+	tests := map[string]test{
+		"nil":              {input: nil, want: "[]"},
+		"strSlicePtrNil":   {input: strSlicePtrNil, want: "[]"},
+		"strSlice":         {input: []string{"A", "B"}, want: `["A","B"]`},
+		"strSlicePtr":      {input: &[]string{"A", "B"}, want: `["A","B"]`},
+		"strSlicePtrEmpty": {input: &strSlicePtrEmpty, want: `[]`},
+		"structSlice":      {input: structSlice, want: `[{"name":"x"}]`},
+		"structSlicePtr":   {input: &structSlice, want: `[{"name":"x"}]`},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			nullJSONText, err := NewSliceNullJSONText(tc.input, true)
+			if err != nil {
+				t.Errorf("序列化失败: %s\n", err)
+				return
+			}
+			marshalJSON, err := nullJSONText.MarshalJSON()
+			if err != nil {
+				t.Errorf("获取序列化后json字符串失败: %s\n", err)
+				return
+			}
+			marshalJSONStr := string(marshalJSON)
+			if marshalJSONStr != tc.want {
+				t.Errorf(" excepted:%s, got:%s", tc.want, marshalJSONStr)
+				return
+			}
+		})
+	}
+}
+
+func TestNewSliceJSONText(t *testing.T) {
+	type test struct {
+		input interface{}
+		want  string
+	}
+	var strSlicePtrNil []string
+	var strSlicePtrEmpty = make([]string, 0, 0)
+	var structSlice = []User{{"x"}}
+
+	tests := map[string]test{
+		"nil":              {input: nil, want: "[]"},
+		"strSlicePtrNil":   {input: strSlicePtrNil, want: "[]"},
+		"strSlice":         {input: []string{"A", "B"}, want: `["A","B"]`},
+		"strSlicePtr":      {input: &[]string{"A", "B"}, want: `["A","B"]`},
+		"strSlicePtrEmpty": {input: &strSlicePtrEmpty, want: `[]`},
+		"structSlice":      {input: structSlice, want: `[{"name":"x"}]`},
+		"structSlicePtr":   {input: &structSlice, want: `[{"name":"x"}]`},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			nullJSONText, err := NewSliceJSONText(tc.input, true)
+			if err != nil {
+				t.Errorf("序列化失败: %s\n", err)
+				return
+			}
+			marshalJSON, err := nullJSONText.MarshalJSON()
+			if err != nil {
+				t.Errorf("获取序列化后json字符串失败: %s\n", err)
+				return
+			}
+			marshalJSONStr := string(marshalJSON)
+			if marshalJSONStr != tc.want {
+				t.Errorf(" excepted:%s, got:%s", tc.want, marshalJSONStr)
+				return
+			}
+		})
+	}
+}
