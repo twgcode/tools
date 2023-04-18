6a7840aa799ebe4ba7eb12c1ee74b462464d4334
wei-G <wei_g_it@163.com>
2023-04-18 14:21:43 +0800

    feat(byteconv): 新增 字节和 GiB的互相转换函数

--- byteconv/byteconv.go
+++ byteconv/byteconv.go
@@ -7,9 +7,19 @@
 
 package byteconv
 
+// GiBToByte 将GiB转换为字节
+func GiBToByte(input int64) int64 {
+	return input * 1024 * 1024 * 1024
+}
+
+// ByteToGiB 将字节转换为GiB
+func ByteToGiB(input int64) float64 {
+	return float64(input) / 1024 / 1024 / 1024
+}
+
 // GBToByte 将GB转换为字节
-func GBToByte(gb int64) int64 {
-	return gb * 1000 * 1000 * 1000
+func GBToByte(input int64) int64 {
+	return input * 1000 * 1000 * 1000
 }
 
 // ByteToGB 将字节转换为GB
