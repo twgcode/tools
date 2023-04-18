33c2a4b6d07407e6230bcaf75a11e6ba9410e087
wei-G <wei_g_it@163.com>
2023-04-18 14:33:41 +0800

    feat(byteconv): 新增 字节和 MiB、KiB、MB、KB的互相转换函数

--- byteconv/byteconv.go
+++ byteconv/byteconv.go
@@ -7,22 +7,62 @@
 
 package byteconv
 
-// GiBToByte 将GiB转换为字节
+// GiBToByte 将 GiB 转换为 字节
 func GiBToByte(input int64) int64 {
 	return input * 1024 * 1024 * 1024
 }
 
-// ByteToGiB 将字节转换为GiB
+// ByteToGiB 将 字节 转换为 GiB
 func ByteToGiB(input int64) float64 {
 	return float64(input) / 1024 / 1024 / 1024
 }
 
-// GBToByte 将GB转换为字节
+// MiBToByte 将 MiB 转换为 字节
+func MiBToByte(input int64) int64 {
+	return input * 1024 * 1024
+}
+
+// ByteToMiB 将 字节 转换为 MiB
+func ByteToMiB(input int64) float64 {
+	return float64(input) / 1024 / 1024
+}
+
+// KiBToByte 将 KiB 转换为 字节
+func KiBToByte(input int64) int64 {
+	return input * 1024
+}
+
+// ByteToKiB 将 字节 转换为 KiB
+func ByteToKiB(input int64) float64 {
+	return float64(input) / 1024
+}
+
+// GBToByte 将 GB 转换为 字节
 func GBToByte(input int64) int64 {
 	return input * 1000 * 1000 * 1000
 }
 
-// ByteToGB 将字节转换为GB
+// ByteToGB 将 字节 转换为 GB
 func ByteToGB(input int64) float64 {
 	return float64(input) / 1000 / 1000 / 1000
+}
+
+// MBToByte 将 MB 转换为 字节
+func MBToByte(input int64) int64 {
+	return input * 1000 * 1000
+}
+
+// ByteToMB 将 字节 转换为 MB
+func ByteToMB(input int64) float64 {
+	return float64(input) / 1000 / 1000
+}
+
+// KBToByte 将 KB 转换为 字节
+func KBToByte(input int64) int64 {
+	return input * 1024
+}
+
+// ByteToKB 将 字节 转换为 KB
+func ByteToKB(input int64) float64 {
+	return float64(input) / 1024
 }
